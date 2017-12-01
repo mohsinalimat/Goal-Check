16c983588f7e5d0ff8f7e52754a492b0a6914ff9
alwongg <wong_1994@hotmail.com>
2017-11-30 20:27:39 -0600

    Goal cell UI and class file

---
+++ Goal Check/View/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  Goal Check
+//
+//  Created by Alex Wong on 11/30/17.
+//  Copyright © 2017 Alex Wong. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescription: UILabel!
+    
+    @IBOutlet weak var goalType: UILabel!
+    
+    @IBOutlet weak var goalProgress: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescription.text = description
+        self.goalType.text = type
+        self.goalProgress.text = String(describing: goalProgressAmount)
+    }
+}
