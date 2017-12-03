8910bce73885ce94ecb8a12932e471081360a8e2
alwongg <wong_1994@hotmail.com>
2017-12-03 15:38:26 -0600

    Fix dismissal segues from FinishGoalVC

--- Goal Check/Controller/CreateGoalVC.swift
+++ Goal Check/Controller/CreateGoalVC.swift
@@ -52,7 +52,7 @@ class CreateGoalVC: UIViewController, UITextViewDelegate {
         if goalTextView.text != "" && goalTextView.text != "What is your goal?"{
             guard let finishGoalVC = storyboard?.instantiateViewController(withIdentifier: "finishGoalVC") as? FinishGoalVC else {return}
             finishGoalVC.initData(description: goalTextView.text!, type: goalType)
-            presentDetail(finishGoalVC)
+            presentingViewController?.presentSecondaryDetail(finishGoalVC)
         }
     }
     
